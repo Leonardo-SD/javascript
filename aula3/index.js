0a6a582ba940e2f1cbdf5123bf0a515cc747b924
Leonardo Souza Diirr <117363427+Leonardo-SD@users.noreply.github.com>
2022-12-02 18:21:41 -0300

    js

---
+++ aula3/index.js
@@ -0,0 +1,4 @@
+console.log('Hello world!');
+console.log(4 + 32);
+
+console.log('Meu nome é "Leonardo souza Diirr". Estou aprendendo JavaScript às', 5, 'da tarde.');
---
+++ aula4/index.js
@@ -0,0 +1,7 @@
+// <- Usado para comentar em JavaScript
+console.log('Olá Mundo!') // exêmplo de comentário
+
+/* 
+Usado para comentários maiores
+*/
+
