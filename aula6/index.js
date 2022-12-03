6240d491a565c63cc62ca885dcf59edd15c7a9d2
Leonardo Souza Diirr <117363427+Leonardo-SD@users.noreply.github.com>
2022-12-03 19:07:28 -0300

    js

---
+++ aula6/index.js
@@ -0,0 +1,6 @@
+let nome = 'Lucas'
+
+console.log(nome, 'nasceu em 1984.')
+console.log('em 2000', nome, 'conheceu Maria.')
+console.log(nome,'casou-se com Maria em 2012.')
+console.log('Maria teve 1 filho com',nome, 'em 2015.')
---
+++ exercicios/ex1.js
@@ -0,0 +1,11 @@
+const nome = 'Luiz Otávio'
+const sobrenome = 'Miranda'
+const idade = 30
+const peso = 84
+const altura = 1.80
+let imc;
+imc = peso / (altura * altura)
+let anoNascimento;
+anoNascimento = 2019 - idade
+
+console.log(`${nome} ${sobrenome} tem  ${idade} anos, pesa ${peso} kg, tem ${altura} de altura e seu IMC é de  ${imc} . ${nome} nasceu em ${anoNascimento}.`);
