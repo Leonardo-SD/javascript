1548890d7a48c2efa9c4c5db9324a71cd36ad134
Leonardo Souza Diirr <117363427+Leonardo-SD@users.noreply.github.com>
2022-12-19 21:04:59 -0300

    js

---
+++ aula21/index.js
@@ -0,0 +1,19 @@
+/*
+        Operadores de comparação
+
+> Maior que
+>= Maior que ou igual a
+< Menor que
+<= Menor que ou igual a
+=== Igualdade estrita (Valor e tipo) preferivel
+!== Diferente estrito (Valor e tipo) preferivel
+
+        Operadores lógicos
+
+&& -> AND -> E  -> Todas expressões precisam ser verdadeiras para retornar true
+|| -> OR  -> OU -> Só uma das expreções precisam ser verdadeiras para retornar true
+!  -> NOT -> NÃO -> Vai negar o valor, invertendo ele 
+
+*/
+
+
